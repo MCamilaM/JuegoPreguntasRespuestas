812e9f1d18ccb9c86a0d3de2c4f9332ac4a1361f
Camila Morales <camila.moralesg21@gmail.com>
2022-01-09 21:17:12 -0500

    Clases Categoria y Pregunta desarrolladas

--- JuegoPreguntasRespuestas/src/modelo/Categoria.java
+++ JuegoPreguntasRespuestas/src/modelo/Categoria.java
@@ -4,10 +4,105 @@
  */
 package modelo;
 
+import java.util.List;
+
 /**
  *
  * @author Camila Morales
  */
 public class Categoria {
+    /**
+     * Atributos
+     */
+    private int idCategoria;
+    private String descripción;
+    private String nivelDificultad;
+    private List<Pregunta> preguntas;
+
+    /**
+     * Constructor vacío
+     */
+    public Categoria() {
+    }
+
+    /**
+     * Constructor 
+     * @param descripción
+     * @param nivelDificultad 
+     */
+    public Categoria(String descripción, String nivelDificultad) {
+        this.descripción = descripción;
+        this.nivelDificultad = nivelDificultad;
+    }
+    
+    /**
+     * Constructor 
+     * @param idCategoria
+     * @param descripción
+     * @param nivelDificultad 
+     */
+    public Categoria(int idCategoria, String descripción, String nivelDificultad) {
+        this.idCategoria = idCategoria;
+        this.descripción = descripción;
+        this.nivelDificultad = nivelDificultad;
+    }
+
+    /**
+     * 
+     * @return idCategoria
+     */
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    /**
+     * 
+     * @param idCategoria 
+     */
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    /**
+     * 
+     * @return descripcion
+     */
+    public String getDescripción() {
+        return descripción;
+    }
+
+    /**
+     * 
+     * @param descripción 
+     */
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+
+    /**
+     * 
+     * @return nivelDificultad
+     */
+    public String getNivelDificultad() {
+        return nivelDificultad;
+    }
+
+    /**
+     * 
+     * @param nivelDificultad 
+     */
+    public void setNivelDificultad(String nivelDificultad) {
+        this.nivelDificultad = nivelDificultad;
+    }
+
+    public List<Pregunta> getPreguntas() {
+        return preguntas;
+    }
+
+    public void setPreguntas(List<Pregunta> preguntas) {
+        this.preguntas = preguntas;
+    }
+    
+    
     
 }
--- JuegoPreguntasRespuestas/src/modelo/Pregunta.java
+++ JuegoPreguntasRespuestas/src/modelo/Pregunta.java
@@ -9,5 +9,88 @@ package modelo;
  * @author Camila Morales
  */
 public class Pregunta {
+    /**
+     * Atributos
+     */
+    private int idPregunta;
+    private String descripcion;
+    private int idCategoria;
+
+    /**
+     * Constructor vacío
+     */
+    public Pregunta() {
+    }
+
+    /**
+     * Constructor
+     * @param descripcion
+     * @param idCategoria 
+     */
+    public Pregunta(String descripcion, int idCategoria) {
+        this.descripcion = descripcion;
+        this.idCategoria = idCategoria;
+    }    
+    
+    /**
+     * Constructor
+     * @param idPregunta
+     * @param descripcion
+     * @param idCategoria 
+     */
+    public Pregunta(int idPregunta, String descripcion, int idCategoria) {
+        this.idPregunta = idPregunta;
+        this.descripcion = descripcion;
+        this.idCategoria = idCategoria;
+    }
+
+    /**
+     * 
+     * @return idPregunta
+     */
+    public int getIdPregunta() {
+        return idPregunta;
+    }
+
+    /**
+     * 
+     * @param idPregunta 
+     */
+    public void setIdPregunta(int idPregunta) {
+        this.idPregunta = idPregunta;
+    }
+
+    /**
+     * 
+     * @return descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * 
+     * @param descripcion 
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * 
+     * @return idCategoria
+     */
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    /**
+     * 
+     * @param idCategoria 
+     */
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+    
     
 }
